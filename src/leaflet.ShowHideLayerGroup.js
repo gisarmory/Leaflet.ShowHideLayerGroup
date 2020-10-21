72d508d135a5676cc1496baaac7c4bf47e26d45c
xuyf <xuyf_90@163.com>
2020-10-21 18:29:38 +0800

    优化代码

--- src/leaflet.ShowHideLayerGroup.js
+++ src/leaflet.ShowHideLayerGroup.js
@@ -1,45 +1,43 @@
 L.LayerGroup.include({
-    /**
-     * 隐藏，2020年09月17日，xuyf
-     */
-    hideLayer: function() {
-        this.eachLayer(function(itemLayer) {
-            if (itemLayer._icon) {
-                itemLayer.setOpacity(0)
-            } else {
-                if(itemLayer.options.opacity){
-                    itemLayer.options.opacityBefore = itemLayer.options.opacity
-                } else {
-                    itemLayer.options.opacityBefore = 1
-                }
-                if(itemLayer.options.fillOpacity){
-                    itemLayer.options.fillOpacityBefore = itemLayer.options.fillOpacity
-                } else {
-                    itemLayer.options.fillOpacityBefore = 1
-                }
-                itemLayer.setStyle({
-                    opacity: 0,
-                    fillOpacity: 0
-                })
-            }
-        })
-    },
-    /**
-     * 显示，2020年09月17日，xuyf
-     */
-    showLayer: function() {
-        this.eachLayer(function(itemLayer) {
-            if (itemLayer._icon) {
-                itemLayer.setOpacity(1)
-            } else {
-                if(itemLayer.options.opacity || itemLayer.options.fillOpacity){
-                    return
-                }
-                itemLayer.setStyle({
-                    opacity: itemLayer.options.opacityBefore,
-                    fillOpacity: itemLayer.options.fillOpacityBefore
-                })
-            }
-        })
-    }
+  // 隐藏
+  hideLayer: function() {
+      this.eachLayer(function(itemLayer) {
+          // 记录默认时opacity值，以便显示时样式一致
+          if(itemLayer.options.opacity){
+              itemLayer.options.opacityBefore = itemLayer.options.opacity
+          } else {
+              itemLayer.options.opacityBefore = 1
+          }
+          if(itemLayer.options.fillOpacity){
+              itemLayer.options.fillOpacityBefore = itemLayer.options.fillOpacity
+          } else {
+              itemLayer.options.fillOpacityBefore = 1
+          }
+
+          if (itemLayer._icon) {
+              itemLayer.setOpacity(0)
+          } else {
+              itemLayer.setStyle({
+                  opacity: 0,
+                  fillOpacity: 0
+              })
+          }
+      })
+  },
+  // 显示
+  showLayer: function() {
+      this.eachLayer(function(itemLayer) {
+          if(itemLayer.options.opacity || itemLayer.options.fillOpacity){
+              return
+          }
+          if (itemLayer._icon) {
+              itemLayer.setOpacity(itemLayer.options.opacityBefore)
+          } else {
+              itemLayer.setStyle({
+                  opacity: itemLayer.options.opacityBefore,
+                  fillOpacity: itemLayer.options.fillOpacityBefore
+              })
+          }
+      })
+  }
 });
